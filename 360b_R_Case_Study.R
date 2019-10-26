0bb858803787b3739ee993678d207cfc9887a72b
ksatola <krzysztof@satola.net>
2019-10-26 22:55:13 +0200

    Update

---
+++ 360b_R_Case_Study.R
@@ -0,0 +1,125 @@
+
+# --------------------------
+# Analyzing Flight Data ----
+# --------------------------
+
+# Load tidyverse ----
+install.packages("tidyverse")
+library("tidyverse")
+
+# Load flights data frame ----
+install.packages("nycflights13")
+library("nycflights13")
+
+# Getting to know the flights data set ----
+
+# Read available documentation
+?flights
+
+# Check the number of rows and columns
+dim(flights)
+
+# Inspect the column names
+colnames(flights)
+
+# Look at the data frame in the RStudio Viewer
+View(flights)
+
+head(flights)
+
+# Questions ----
+# 1. Which airline has the highest number of delayed departures? ----
+
+?n
+
+has_most_delays <- flights %>%
+  group_by(carrier) %>%
+  filter(dep_delay > 0) %>%
+  summarize(num_delay = n()) %>%
+  filter(num_delay == max(num_delay)) %>%
+  select(carrier)
+
+print(as.data.frame(has_most_delays))
+
+View(airlines)
+
+# Get name of the most delayed carrier
+most_delayed_name <- has_most_delays %>%
+  left_join(airlines, by = 'carrier') %>%
+  select(name)
+
+print(most_delayed_name$name)
+
+# We should check the proportion of delayed flights the have more fair answer
+# as UA is a big airline
+
+# number of all flights per carrier
+all_flight_stats <- flights %>%
+  group_by(carrier) %>%
+  summarize(
+    num_all_flights = n()
+  )
+
+# number of delayed flights per carrier
+delay_flight_stats <- flights %>%
+  group_by(carrier) %>%
+  filter(dep_delay > 0) %>%
+  summarize(
+    num_delay = n()
+  )
+
+# combine per carrier summaries
+flight_stats <- all_flight_stats %>%
+  left_join(delay_flight_stats, by = "carrier")
+
+# add ratio column
+flight_stats <- flight_stats %>%
+  mutate(
+    ratio = flight_stats$num_delay / flight_stats$num_all_flights
+  )
+
+has_most_delays_by_ratio <- flight_stats %>%
+  filter(ratio == max(ratio)) %>%
+  select(carrier, ratio)
+
+print(has_most_delays_by_ratio)
+
+# Get name of the most delayed carrier
+most_delayed_name_by_ratio <- has_most_delays_by_ratio %>%
+  left_join(airlines, by = 'carrier') %>%
+  select(name)
+
+print(most_delayed_name_by_ratio)
+
+# It seems, that Southwest Airlines Co. - WN - with more than 53% of delayed flights wins!
+
+# 2. On average, to which airport do flights arrive most early? ----
+
+most_early <- flights %>%
+  group_by(dest) %>%
+  summarize(delay = mean(arr_delay))
+
+print(most_early)
+
+# there are many NAs in the arr_delay column, use na.rm = TRUE to remove them from mean()
+
+most_early <- flights %>%
+  group_by(dest) %>%
+  summarize(delay = mean(arr_delay, na.rm = TRUE))
+
+print(most_early)
+
+View(airports)
+
+most_early <- flights %>%
+  group_by(dest) %>%
+  summarize(delay = mean(arr_delay, na.rm = TRUE)) %>%
+  filter(delay == min(delay, na.rm = TRUE)) %>%
+  select(dest, delay) %>%
+  left_join(airports, by = c("dest" = "faa")) %>%
+  select(dest, name, delay)
+
+print(most_early)
+
+# 3. In which month do flights tend to have the longest delays? ----
+
