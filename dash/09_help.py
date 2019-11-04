94d6beb822b4e3c3aa351170468a10a4830c49ac
ksatola <krzysztof@satola.net>
2019-11-04 23:34:06 +0100

    Update

---
+++ dash/09_help.py
@@ -0,0 +1,8 @@
+#import dash
+#import dash_core_components as dcc
+import dash_html_components as html
+
+print(help(html.Div))
+
+# In terminal (it will generate an html file):
+# $ pydoc -w dash_html_components.Div
---
+++ dash/11_callback2.py
@@ -0,0 +1,51 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
+import pandas as pd
+
+df = pd.read_csv('data/gapminderDataFiveYear.csv')
+
+app = dash.Dash()
+
+
+# https://dash.plot.ly/dash-core-components/dropdown
+# We need to construct a dictionary of dropdown values for the years
+year_options = []
+for year in df['year'].unique():
+    year_options.append({'label':str(year),'value':year})
+
+app.layout = html.Div([
+    dcc.Graph(id='graph'),
+    dcc.Dropdown(id='year-picker',options=year_options,value=df['year'].min())
+])
+
+@app.callback(Output('graph', 'figure'),
+              [Input('year-picker', 'value')])
+def update_figure(selected_year):
+    filtered_df = df[df['year'] == selected_year]
+    traces = []
+    for continent_name in filtered_df['continent'].unique():
+        df_by_continent = filtered_df[filtered_df['continent'] == continent_name]
+        traces.append(go.Scatter(
+            x=df_by_continent['gdpPercap'],
+            y=df_by_continent['lifeExp'],
+            text=df_by_continent['country'],
+            mode='markers',
+            opacity=0.7,
+            marker={'size': 15},
+            name=continent_name
+        ))
+
+    return {
+        'data': traces,
+        'layout': go.Layout(
+            xaxis={'type': 'log', 'title': 'GDP Per Capita'},
+            yaxis={'title': 'Life Expectancy'},
+            hovermode='closest'
+        )
+    }
+
+if __name__ == '__main__':
+    app.run_server()
